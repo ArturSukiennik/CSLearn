30eb2089c990098d483592275ae923eee2cc9ee9
ArturSukiennik <artursukiennik@gmail.com>
2023-02-07 09:33:42 +0100

    Add files via upload

---
+++ Worker.cs
@@ -0,0 +1,27 @@
+namespace HostBuilder
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
+
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var title = _configuration.GetSection("Settings")["AppMainTitle"];
+
+            _logger.LogInformation($"{title} welcomes you.");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
